9a39b6188a8829d7a5f96695add24b0764d8f02a
jergauth <jeremiegthr@gmail.com>
2023-08-20 17:54:28 +0200

    feat(comp): add progress-bar

---
+++ src/stories/starcraft/starcraft 1/progress-bar/ProgressBar.stories.tsx
@@ -0,0 +1,45 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { ProgressBar } from './ProgressBar';
+
+const meta = {
+  title: 'Starcraft/Starcraft 1/ProgressBar',
+  component: ProgressBar,
+  decorators: [
+    (Story) => (
+      <div
+        style={{
+          width: 200,
+          height: 40,
+        }}
+      >
+        <Story />
+      </div>
+    ),
+  ],
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  argTypes: {
+    percent: {
+      control: 'range',
+      min: 0,
+      max: 100,
+    },
+    color: {
+      control: 'select',
+      options: ['red', 'orange', 'green'],
+    },
+  },
+} satisfies Meta<typeof ProgressBar>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+  args: {
+    score: 7750,
+    percent: 42,
+    color: 'red',
+  },
+};
---
+++ src/stories/starcraft/starcraft 1/progress-bar/ProgressBar.tsx
@@ -0,0 +1,44 @@
+import clsx from 'clsx';
+import { Typography } from '../typography/Typography';
+
+interface ProgressBarProps {
+  /**
+   * Score displayed on the bar
+   */
+  score: number;
+  /**
+   * Bar progression
+   */
+  percent: number;
+  /**
+   * Bar color
+   */
+  color: 'red' | 'orange' | 'green';
+}
+
+/**
+ * Starcraft 1 typography in menus
+ */
+export const ProgressBar = ({ color, score, percent }: ProgressBarProps) => {
+  return (
+    <div
+      className={clsx('w-full h-full bg-black relative border-2 p-1 rounded', {
+        'border-orange-800': color === 'orange',
+        'border-red-800': color === 'red',
+        'border-green-800': color === 'green',
+      })}
+    >
+      <div
+        className={clsx(`h-full`, {
+          'bg-orange-800': color === 'orange',
+          'bg-red-800': color === 'red',
+          'bg-green-800': color === 'green',
+        })}
+        style={{ width: `${percent}%` }}
+      />
+      <p className="absolute inset-0 flex items-center justify-center">
+        <Typography text={score.toString()} variant="secondary" />
+      </p>
+    </div>
+  );
+};
